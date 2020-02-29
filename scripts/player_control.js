c07dfc19ffdff653cbbd4a0462575f6323b5b5fd
KarlKKang <karl_kai.kang@icloud.com>
2020-02-29 04:20:52 -0800

    Improve code format consistency

--- scripts/player_control.js
+++ scripts/player_control.js
@@ -1,21 +1,21 @@
-function resolutionSwitch (dropdown) {
-  var resolution = dropdown.options[dropdown.selectedIndex].value;
-  var videoSource = dropdown.parentNode.parentNode.getElementsByTagName("video")[0].getElementsByTagName("source")[0];
-  var videoSourceDirectory = videoSource.src;
-  var newVideoSourceDirectory;
+function resolutionSwitch(dropdown) {
+    var resolution = dropdown.options[dropdown.selectedIndex].value;
+    var videoSource = dropdown.parentNode.parentNode.getElementsByTagName("video")[0].getElementsByTagName("source")[0];
+    var videoSourceDirectory = videoSource.src;
+    var newVideoSourceDirectory;
 
-  newVideoSourceDirectory = generateNewDirectory (videoSourceDirectory, resolution);
-  videoSource.setAttribute("src", newVideoSourceDirectory);
+    newVideoSourceDirectory = generateNewDirectory(videoSourceDirectory, resolution);
+    videoSource.setAttribute("src", newVideoSourceDirectory);
 
-  videoSource.parentNode.load();
+    videoSource.parentNode.load();
 }
 
 
-function generateNewDirectory (currentDirectory, resolution) {
-  var newDirectory;
+function generateNewDirectory(currentDirectory, resolution) {
+    var newDirectory;
 
-  newDirectory = currentDirectory.replace("1080p", resolution);
-  newDirectory = newDirectory.replace("2160p", resolution);
+    newDirectory = currentDirectory.replace("1080p", resolution);
+    newDirectory = newDirectory.replace("2160p", resolution);
 
-  return newDirectory;
+    return newDirectory;
 }
