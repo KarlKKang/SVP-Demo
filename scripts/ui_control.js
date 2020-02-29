67dcea3195f173a3949053c10f2dfcac4e5d0cc6
KarlKKang <karl_kai.kang@icloud.com>
2020-02-29 04:11:12 -0800

    Improve loading screen

--- scripts/ui_control.js
+++ scripts/ui_control.js
@@ -1,43 +1,42 @@
 function updateBackground() {
-  var ratio = window.innerHeight/window.innerWidth;
-  if (mobileCheck()){
-    document.getElementById("mainBody").style.background = "#323232";
-  }
-  else if (ratio>4/3) {
-    document.getElementById("mainBody").style.background = "url(https://storage.googleapis.com/svp-demo/images/DP_0011_veED_009_1620x2160.png) no-repeat center center";
-  }
-  else {
-    document.getElementById("mainBody").style.background = "url(https://storage.googleapis.com/svp-demo/images/DP_0011_veED_009_3840x2160.jpg) no-repeat center center";
-  }
-  document.getElementById("mainBody").style.backgroundAttachment = "fixed";
-  document.getElementById("mainBody").style.backgroundSize = "cover";
+    var ratio = window.innerHeight / window.innerWidth;
+    if (mobileCheck()) {
+        document.getElementById("mainBody").style.background = "#323232";
+    } else if (ratio > 4 / 3) {
+        document.getElementById("mainBody").style.background = "url(https://storage.googleapis.com/svp-demo/images/DP_0011_veED_009_1620x2160.png) no-repeat center center";
+    } else {
+        document.getElementById("mainBody").style.background = "url(https://storage.googleapis.com/svp-demo/images/DP_0011_veED_009_3840x2160.jpg) no-repeat center center";
+    }
+    document.getElementById("mainBody").style.backgroundAttachment = "fixed";
+    document.getElementById("mainBody").style.backgroundSize = "cover";
 }
 
 
 function dropdown() {
-  var acc = document.getElementsByClassName("accordion");
-  var i;
-  
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  }
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
 }
 
 
 function mobileCheck() {
-  var isMobile = /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/i.test(navigator.userAgent);
-  return isMobile;
+    var isMobile = /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/i.test(navigator.userAgent);
+    return isMobile;
 }
 
 window.addEventListener('load', function () {
-  var loadingScreen = document.getElementById("loadingScreen");
-  loadingScreen.style.display = "none";
+    setTimeout(function () {
+        document.getElementById("loadingScreen").style.display = "none";
+    }, 1500)
 })
